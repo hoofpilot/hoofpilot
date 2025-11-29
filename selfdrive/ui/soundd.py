00c07958867365cabbfd9e1f77007921f4e8d251
amoghmunikote <amogh.munikote@gmail.com>
2025-11-29 15:58:44 -0800

    Adjusted sample rate

--- selfdrive/ui/soundd.py
+++ selfdrive/ui/soundd.py
@@ -14,10 +14,10 @@ from openpilot.common.swaglog import cloudlog
 from openpilot.system import micd
 from openpilot.sunnypilot.selfdrive.ui.quiet_mode import QuietMode
 
-SAMPLE_RATE = 48000
+SAMPLE_RATE = 44100
 SAMPLE_BUFFER = 4096 # (approx 100ms)
 MAX_VOLUME = 1.0
-MIN_VOLUME = 0.2
+MIN_VOLUME = 0.3
 SELFDRIVE_STATE_TIMEOUT = 5 # 5 seconds
 FILTER_DT = 1. / (micd.SAMPLE_RATE / micd.FFT_SAMPLES)
 
