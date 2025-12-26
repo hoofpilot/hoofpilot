82721feacc8e7a6b03b0e247fd9f431592c53292
Amogh Munikote <amogh.munikote@gmail.com>
2025-12-26 10:55:54 -0800

    Merging master into asset simplification (#31)
    * Updated git lfs
    
    * Pushing updated git lfs files
    
    * CAN ignition priority
    
    * add ignition_state.py and refactor ignition logic
    
    * remove unused import
    
    * Added new symlinks
    
    * Removed symlinks from lfs
    
    * Updated lfs files
    
    * One more lfs update
    
    * Updated lfs files more time
    
    * Readded symbolic links
    
    * Update ONNX model path in big_driving_policy
    
    * Update path for big_driving_vision.onnx
    
    * Update library path for libqpOASES_e.so
    
    * Update library path for libqpOASES_e.dylib
    
    * Update path for libqpOASES_e.so
    
    * oops i added whitespace
    
    ---------
    
    Co-authored-by: mazdanick <nick.tompk@gmail.com>

--- system/manager/manager.py
+++ system/manager/manager.py
@@ -6,7 +6,6 @@ import sys
 import time
 import traceback
 
-from cereal import log
 import cereal.messaging as messaging
 import openpilot.system.sentry as sentry
 from openpilot.common.utils import atomic_write
@@ -20,6 +19,7 @@ from openpilot.system.athena.registration import register, UNREGISTERED_DONGLE_I
 from openpilot.common.swaglog import cloudlog, add_file_handler
 from openpilot.system.version import get_build_metadata
 from openpilot.system.hardware.hw import Paths
+from openpilot.system.hardware.ignition_state import ignition_state
 
 
 def manager_init() -> None:
@@ -143,7 +143,7 @@ def manager_thread() -> None:
     elif not started and started_prev:
       params.clear_all(ParamKeyFlag.CLEAR_ON_OFFROAD_TRANSITION)
 
-    ignition = any(ps.ignitionLine or ps.ignitionCan for ps in sm['pandaStates'] if ps.pandaType != log.PandaState.PandaType.unknown)
+    ignition = ignition_state.update(sm['pandaStates'])
     if ignition and not ignition_prev:
       params.clear_all(ParamKeyFlag.CLEAR_ON_IGNITION_ON)
 
