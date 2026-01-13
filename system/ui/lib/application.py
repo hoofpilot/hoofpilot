75445dcd46c70d8e4b20dd3ac90bf48f52a7887f
amoghmunikote <amogh.munikote@gmail.com>
2026-01-12 18:50:08 -0800

    Blur reduction

--- system/ui/lib/application.py
+++ system/ui/lib/application.py
@@ -600,7 +600,10 @@ class GuiApplication(GuiApplicationExt):
     for font_weight_file in FontWeight:
       with as_file(FONT_DIR) as fspath:
         fnt_path = fspath / font_weight_file
-        font = rl.load_font(fnt_path.as_posix())
+        if font_weight_file == FontWeight.JETBRAINS:
+          font = rl.load_font_ex(fnt_path.as_posix(), 200, None, 0)
+        else:
+          font = rl.load_font(fnt_path.as_posix())
         if font_weight_file != FontWeight.UNIFONT:
           rl.set_texture_filter(font.texture, rl.TextureFilter.TEXTURE_FILTER_BILINEAR)
         self._fonts[font_weight_file] = font
