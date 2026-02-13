6e7d9795a7ccba0975f144a351f9ad26c889fcdf
amoghmunikote <amogh.munikote@gmail.com>
2026-02-12 22:56:19 -0800

    Removing copyright

--- selfdrive/ui/sunnypilot/layouts/settings/vehicle/brands/factory.py
+++ selfdrive/ui/sunnypilot/layouts/settings/vehicle/brands/factory.py
@@ -1,9 +1,3 @@
-"""
-Copyright (c) 2021-, Haibin Wen, sunnypilot, and a number of other contributors.
-
-This file is part of sunnypilot and is licensed under the MIT License.
-See the LICENSE.md file in the root directory for more details.
-"""
 from openpilot.selfdrive.ui.sunnypilot.layouts.settings.vehicle.brands.base import BrandSettings
 from openpilot.selfdrive.ui.sunnypilot.layouts.settings.vehicle.brands.body import BodySettings
 from openpilot.selfdrive.ui.sunnypilot.layouts.settings.vehicle.brands.chrysler import ChryslerSettings
--- sunnypilot/mapd/mapd_manager.py
+++ sunnypilot/mapd/mapd_manager.py
@@ -1,10 +1,4 @@
 #!/usr/bin/env python3
-"""
-Copyright (c) 2021-, Haibin Wen, sunnypilot, and a number of other contributors.
-
-This file is part of sunnypilot and is licensed under the MIT License.
-See the LICENSE.md file in the root directory for more details.
-"""
 import json
 import platform
 import os
