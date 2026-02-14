d8c00a07b4af2456a5b45d463c79cfd81fa339f3
amoghmunikote <amogh.munikote@gmail.com>
2026-02-14 11:48:51 -0800

    Updated PIN control

--- selfdrive/ui/sunnypilot/layouts/settings/stable.py
+++ selfdrive/ui/sunnypilot/layouts/settings/stable.py
@@ -1,14 +1,14 @@
 ﻿import hashlib
+import hashlib
 import hmac
 import os
 
 from openpilot.selfdrive.ui.ui_state import ui_state
 from openpilot.system.ui.lib.multilang import tr
 from openpilot.system.ui.sunnypilot.widgets.input_dialog import InputDialogSP
-from openpilot.system.ui.sunnypilot.widgets.list_view import dual_button_item_sp, button_item_sp, toggle_item_sp, Spacer
+from openpilot.system.ui.sunnypilot.widgets.list_view import button_item_sp, toggle_item_sp
 from openpilot.system.ui.widgets import DialogResult
-from openpilot.system.ui.widgets.confirm_dialog import ConfirmDialog, alert_dialog
-from openpilot.system.ui.widgets.button import ButtonStyle, Button
+from openpilot.system.ui.widgets.confirm_dialog import alert_dialog
 from openpilot.system.ui.widgets import Widget
 from openpilot.system.ui.widgets.scroller_tici import Scroller
 
@@ -23,22 +23,15 @@ class StableLayout(Widget):
       ui_state.params.put_bool("RemoteSshEnabled", False)
       ui_state.params.put_bool("LiveView", False)
 
-    self._reset_pin_buttons = dual_button_item_sp(
-      left_text=lambda: tr("Reset PIN"),
-      right_text=lambda: "",
-      left_callback=self._on_reset_pin_pressed,
-      right_callback=None,
-      enabled=lambda: ui_state.is_offroad() and self._remote_pin_is_set(),
-      border_radius=20,
-    )
-    self._reset_pin_btn: Button = self._reset_pin_buttons.action_item.left_button
-    self._reset_pin_btn.set_button_style(ButtonStyle.DANGER)
-    self._reset_pin_buttons.action_item.right_button.set_visible(False)
-
     self._remote_pin_button = button_item_sp(
       title=lambda: tr("PIN"),
       button_text=self._remote_pin_button_text,
-      description=lambda: tr("Set or change the PIN required to use Remote SSH and Live View from Konik Stable."),
+      description=lambda: (
+        tr("Set or change the PIN required to use Remote SSH and Live View from Konik Stable.") + " " +
+        (f"<b>{tr('The PIN is currently configured.')}</b>"
+         if self._remote_pin_is_set()
+         else f"<b>{tr('The PIN is not currently configured.')}</b>")
+      ),
       callback=self._on_remote_pin_pressed,
       enabled=lambda: ui_state.is_offroad(),
     )
@@ -60,8 +53,7 @@ class StableLayout(Widget):
       enabled=lambda: self._remote_pin_is_set(),
     )
 
-    # put destructive reset at the bottom like other device controls
-    self._scroller = Scroller([self._remote_pin_button, self._live_view_toggle, self._remote_ssh_toggle, Spacer(10), self._reset_pin_buttons],
+    self._scroller = Scroller([self._remote_pin_button, self._live_view_toggle, self._remote_ssh_toggle],
                               line_separator=True, spacing=0)
 
   @staticmethod
@@ -177,27 +169,14 @@ class StableLayout(Widget):
       if not self._remote_pin_verify(old_pin):
         self._show_alert(tr("Wrong PIN."))
         return
-      prompt_pin(tr("PIN"), tr("Enter new PIN (4-12 digits)"), 4, handle_new_pin)
+      # Allow empty input to clear the PIN.
+      prompt_pin(tr("PIN"), tr("Enter new PIN (4-12 digits) or leave blank to clear"), 0, handle_new_pin)
 
     if self._remote_pin_is_set():
       prompt_pin(tr("PIN"), tr("Enter current PIN"), 4, handle_old_pin)
     else:
       # min_text_size=0 so the user can submit empty to clear and return to factory state
       prompt_pin(tr("PIN"), tr("Enter new PIN (4-12 digits) or leave blank to clear"), 0, handle_new_pin)
-
-  def _on_reset_pin_pressed(self) -> None:
-    if not self._remote_pin_is_set():
-      return
-    from openpilot.system.ui.lib.application import gui_app
-
-    dlg = ConfirmDialog(tr("Reset PIN?"), tr("Reset"), tr("Cancel"))
-
-    def cb(res: DialogResult):
-      if res == DialogResult.CONFIRM:
-        self._remote_pin_clear()
-        self._show_alert(tr("PIN reset."))
-
-    gui_app.set_modal_overlay(dlg, cb)
 
   @staticmethod
   def _on_live_view_toggled(enabled: bool):
