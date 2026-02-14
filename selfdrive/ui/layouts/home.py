6eb679ee6819d4ed6139b8f459ccad649cae8825
amoghmunikote <amogh.munikote@gmail.com>
2026-02-14 11:11:42 -0800

    Trying out trips widget

--- selfdrive/ui/layouts/home.py
+++ selfdrive/ui/layouts/home.py
@@ -7,6 +7,7 @@ from openpilot.common.params import Params
 from openpilot.selfdrive.ui.widgets.offroad_alerts import UpdateAlert, OffroadAlert
 from openpilot.selfdrive.ui.widgets.exp_mode_button import ExperimentalModeButton
 from openpilot.selfdrive.ui.widgets.setup import SetupWidget
+from openpilot.selfdrive.ui.widgets.trips import TripsWidget
 from openpilot.system.ui.lib.text_measure import measure_text_cached
 from openpilot.system.ui.lib.application import gui_app, FontWeight, MousePos
 from openpilot.system.ui.lib.multilang import tr, trn
@@ -58,6 +59,7 @@ class HomeLayout(Widget):
     self.alert_notif_rect = rl.Rectangle(0, 0, 220, HEADER_HEIGHT - 10)
 
     self._setup_widget = SetupWidget()
+    self._trips_widget = TripsWidget()
     self._mono_font = None
 
     self._exp_mode_button = ExperimentalModeButton()
@@ -205,9 +207,12 @@ class HomeLayout(Widget):
     self._exp_mode_button.render(self.left_column_rect)
 
   def _render_right_column(self):
-    setup_height = min(520, self.right_column_rect.height)
-    setup_rect = rl.Rectangle(self.right_column_rect.x, self.right_column_rect.y, self.right_column_rect.width, setup_height)
-    self._setup_widget.render(setup_rect)
+    if ui_state.prime_state.is_paired():
+      self._trips_widget.render(self.right_column_rect)
+    else:
+      setup_height = min(520, self.right_column_rect.height)
+      setup_rect = rl.Rectangle(self.right_column_rect.x, self.right_column_rect.y, self.right_column_rect.width, setup_height)
+      self._setup_widget.render(setup_rect)
 
   def _refresh(self):
     self._version_text = self._get_version_text()
