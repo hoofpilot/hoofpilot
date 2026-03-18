9f960491e363fe65f743bb5ce941255796f17caa
amoghmunikote <amogh.munikote@gmail.com>
2026-03-18 14:39:23 -0700

    Updated widget

--- selfdrive/ui/widgets/setup.py
+++ selfdrive/ui/widgets/setup.py
@@ -41,7 +41,7 @@ class SetupWidget(Widget):
     y += 113  # 75 + 38 spacing
 
     # Description
-    desc = tr("Pair your device with comma connect (connect.comma.ai) and claim your comma prime offer.")
+    desc = tr("Pair your device with Konik Stable (stable.konik.ai) to get started.")
     light_font = gui_app.font(FontWeight.NORMAL)
     wrapped = wrap_text(light_font, desc, 50, int(w))
     for line in wrapped:
