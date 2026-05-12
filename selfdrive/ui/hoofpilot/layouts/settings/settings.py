8e158ec1316bd327fe437fedd5583e325a5b4e89
amoghmunikote <amogh.munikote@gmail.com>
2026-05-11 22:00:08 -0700

    Hip hip hooray more bad imports

--- selfdrive/ui/hoofpilot/layouts/settings/settings.py
+++ selfdrive/ui/hoofpilot/layouts/settings/settings.py
@@ -27,7 +27,7 @@ from openpilot.system.ui.lib.application import gui_app, MousePos
 from openpilot.system.ui.lib.multilang import tr_noop
 from openpilot.system.ui.lib.text_measure import measure_text_cached
 from openpilot.system.ui.lib.wifi_manager import WifiManager
-from openpilot.system.ui.sunnypilot.lib.styles import style
+from openpilot.system.ui.hoofpilot.lib.styles import style
 from openpilot.system.ui.widgets import Widget
 from openpilot.system.ui.widgets.scroller_tici import Scroller
 
